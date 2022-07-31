a76417ede06a18d0697e1d2232f84fce7c75a3fd
Gustavo Cavalcante <nowayhecodes@gmail.com>
2022-07-31 02:31:58 -0300

    feat: Map iterator

---
+++ map.go
@@ -0,0 +1,21 @@
+package iters
+
+type mapIterator[T any] struct {
+	source Iterator[T]
+	mapper func(T) T
+}
+
+func (iter *mapIterator[T]) Next() bool {
+	return iter.source.Next()
+}
+
+func (iter *mapIterator[T]) Value() T {
+	value := iter.source.Value()
+	return iter.mapper(value)
+}
+
+func Map[T any](iter Iterator[T], f func(T) T) Iterator[T] {
+	return &mapIterator[T]{
+		iter, f,
+	}
+}
